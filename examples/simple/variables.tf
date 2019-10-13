5341f84302a4a35d37f8f7ff27f109c075e78d7a
nozaq <code@nozaq.io>
2019-10-13 20:42:05 +0900

    misc: add examples

--- examples/simple/variables.tf
+++ examples/simple/variables.tf
@@ -8,12 +8,3 @@ variable "region" {
   description = "The AWS region in which global resources are set up."
   default     = "us-east-1"
 }
-
-variable "management_ip" {
-  description = "The IP address from which an administrator connects to instances via SSH."
-}
-
-variable "key_name" {
-  description = "The key name to use for EC2 instances."
-}
-
